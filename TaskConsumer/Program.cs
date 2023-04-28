0e7253c33dc159246f02dfa41cb96124b1e4fe71
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-04-28 16:28:49 -0500

    chore: initial project setup

---
+++ TaskConsumer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var config = new ConfigurationBuilder()
+.AddJsonFile($"appsettings.development.json")
+.Build();
+
+var rabbitMQConfig = config.GetSection("RabbitMQ");
+
+var factory = new ConnectionFactory
+{
+  HostName = "localhost",
+  Port = int.Parse(rabbitMQConfig.GetSection("Port").Value ?? string.Empty),
+  UserName = rabbitMQConfig.GetSection("Username").Value,
+  Password = rabbitMQConfig.GetSection("Password").Value
+};
+
+using var conn = factory.CreateConnection();
+using var chan = conn.CreateModel();
+
+var queueName = rabbitMQConfig.GetSection("MessageQueue").Value;
+
+chan.QueueDeclare(
+  queue: queueName,
+  durable: false,
+  exclusive: false,
+  autoDelete: false,
+  arguments: null
+);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(chan);
+
+consumer.Received += (model, ea) =>
+{
+  var body = ea.Body.ToArray();
+  var message = Encoding.UTF8.GetString(body);
+
+  Console.WriteLine($" [x] Received {message}");
+
+  int dots = message.Split('.').Length - 1;
+
+  Thread.Sleep(dots * 1000);
+
+  Console.WriteLine(" [x] Done");
+};
+
+chan.BasicConsume(
+  queue: queueName,
+  autoAck: true,
+  consumer: consumer
+  );
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ TaskPublisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+
+var config = new ConfigurationBuilder()
+.AddJsonFile($"appsettings.development.json")
+.Build();
+
+var rabbitMQConfig = config.GetSection("RabbitMQ");
+
+var factory = new ConnectionFactory
+{
+  HostName = "localhost",
+  Port = int.Parse(rabbitMQConfig.GetSection("Port").Value ?? string.Empty),
+  UserName = rabbitMQConfig.GetSection("Username").Value,
+  Password = rabbitMQConfig.GetSection("Password").Value
+};
+
+using var conn = factory.CreateConnection();
+using var chan = conn.CreateModel();
+
+var queueName = rabbitMQConfig.GetSection("MessageQueue").Value;
+
+chan.QueueDeclare(
+  queue: queueName,
+  durable: false,
+  exclusive: false,
+  autoDelete: false,
+  arguments: null
+);
+
+var message = (args.Length > 0)
+? string.Join(" ", args)
+: "Hello World!";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+chan.BasicPublish(
+  exchange: string.Empty,
+  routingKey: queueName,
+  basicProperties: null,
+  body: body
+);
+
+Console.WriteLine($" [x] Sent {message}");
